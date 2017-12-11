08144c3db0a0c5951e0ba0728d628bae7335a3d3
rstaley <staley.robert@gmail.com>
2017-12-10 20:51:33 -0500

    Added Component Three

---
+++ src/app/components/component-three/component-three.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LOGGER } from '../../providers/logger.service';
+import { DatabaseService } from '../../providers/database.service';
+import { getRepository } from 'typeorm';
+import { User } from '../../../entity/User';
+
+@Component({
+  selector: 'app-component-three',
+  templateUrl: './component-three.component.html',
+  styleUrls: ['./component-three.component.scss']
+})
+export class ComponentThreeComponent implements OnInit {
+  title = `Component Two`;
+
+  users: User[] = [];
+
+  //  constructor(private userService: UserService) { 
+  constructor(private router: Router, public databaseService: DatabaseService) {
+
+  }
+
+
+  async ngOnInit() {
+    try {
+      this.users = await getRepository(User).find();
+
+      for (var user of this.users) {
+        LOGGER.info(`User Name in Component Three: ${user.id} ${user.firstName} ${user.lastName}`);
+      }
+    }
+    catch (error) {
+      LOGGER.info(error);
+    }
+  }
+
+  routeToHome(event) {
+    this.router.navigate(['home']);
+  }
+}
